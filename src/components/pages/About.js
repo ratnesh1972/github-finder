0a4fb79cdaaeee0d71bc61bc1052154eee1c7a09
Ratnesh Patil <ratnesh.designs@gmail.com>
2022-03-28 14:01:55 +0530

    About page added

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,11 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>This is about page</h1>
+            <h1>About US</h1>
+            <p>
+                Github Finder is a simple project created with React.js. Using github finger you can type a username and finder will show you all the github profile names that
+                matches your search. You can then view more detailed info of these profile by clicking on "view more". We used github's api to fetch the data of profiles. Also the state management is performed using Reactjs context.
+            </p>
         </div>
     )
 }
